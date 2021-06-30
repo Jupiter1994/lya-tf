90e5bfaf52fc270c707f03b929693f9b4401e637
Jupiter Ding <thejupiterding@gmail.com>
2021-06-29 19:40:03 -0700

    created 1x512x512 fields

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-## Summary
+## Lya-TF
 Mapping baryon properties to Lyman-alpha flux with TensorFlow. Based on the C++ toolkit Gimlet.
 
-## File Descriptions
+## Tutorial
+1. Activate the Conda environment specified by lya-tf.yml.
+2. In `lya_fields.py`, specify the dimensions of the fields you want to compute by specifying `n`. (For example, if you want to compute fields with size 20^3, set `n` to 20.) 
+3. Run `python lya_fields.py` in the command line.
+4. Use `plot_fields.ipynb` (located in `lya_fields`) to visualize the computed fields.
+
+## Contents
+- `lya_fields` replicates the `/apps/lya_fields` application in Gimlet. It contains the main script, `lya_fields.py`, as well as dependencies, results files, and Jupyter notebooks.
 - `lya-tf.yml` is the file specifying this project's Conda environment, which includes TensorFlow 2.4. (NOTE: Python 3.7 and NumPy 1.18 are needed in order to wrap eos-t.f90 in Python.)
 - `data_exploration.ipynb` explores the HDF5 file containing the hydrodynamical simulation results.
-- `scratch_work` contains Python scripts, classes, etc. that replicate Gimlet.
---
+++ lya_fields/snapshot.py
@@ -0,0 +1,109 @@
+import h5py
+import tensorflow as tf
+
+# local modules
+import grid
+import universe
+
+class Snapshot:
+    '''
+    Class that handles a simulation snapshot. 
+
+    '''
+
+    def __init__(self, path):
+        '''
+        Create a snapshot from an HDF5 file.
+        
+        PARAMETERS
+        ----------
+        path: the path to the HDF5 file
+        
+        '''
+        self.file_path = path 
+        snap = h5py.File(self.file_path,'r') # HDF file
+        
+        # Grid/domain properties
+        # (for the example file, these are 1024^3 and 21.09375^3, respectively)
+        self.shape = snap['domain'].attrs['shape'] 
+        self.size = snap['domain'].attrs['size'] 
+        
+        # Universe properties
+        self.z = tf.Variable(snap['universe'].attrs['redshift'])
+        self.scale_factor = tf.divide(1, self.z+1)
+        omega_b = tf.Variable(snap['universe'].attrs['omega_b'])
+        omega_m = tf.Variable(snap['universe'].attrs['omega_m'])
+        omega_l = tf.Variable(snap['universe'].attrs['omega_l'])
+        h = tf.Variable(snap['universe'].attrs['hubble'])
+        sigma_8, n_s = tf.Variable(0.), tf.Variable(0.)
+
+        self.universe = universe.Universe(omega_b, omega_m, omega_l, h, sigma_8, n_s)
+        
+        snap.close()
+        
+    def read_field(self, path):
+        '''
+        Read in a field from the snapshot's source file. Returns a Grid object.
+        
+        PARAMETERS
+        ----------
+        path: the field's path within the file, e.g. '/native_fields/baryon_density'
+        
+        '''
+        snap = h5py.File(self.file_path,'r')
+        field = tf.Variable(snap[path][()]) # convert ndarray to tensor
+
+        snap.close()
+        
+        return grid.Grid(field, self.shape, self.size)
+    
+    def read_field2(self, path, subshape):
+        '''
+        Read in a subsection of a field from (0,0,0). Returns a Grid object.
+        
+        PARAMETERS
+        ----------
+        path: the field's path within the snapshot file
+        subshape: the dimensions of the subsection; should be a list of 3 integers
+        
+        '''
+        # scale factors between the subsection and the full field
+        scale = tf.math.divide(subshape, self.shape)
+        
+        snap = h5py.File(self.file_path,'r')
+        field = snap[path][()] # ndarray
+        field = field[:subshape[0], :subshape[1], :subshape[2]]
+        field = tf.Variable(field, dtype='float64') # ndarray to tensor
+
+        snap.close()
+        
+        return grid.Grid(field, subshape, tf.math.multiply(scale, self.size))
+    
+    def write_field(self, field, path):
+        '''
+        Write a field into the snapshot's source file.
+        
+        PARAMETERS
+        ----------
+        field: a 3D tensor
+        path: the path to write the field into, e.g. '/derived_fields/tau_real'
+        
+        '''
+        snap = h5py.File(self.file_path,'w')
+        data = snap[path]
+        data[...] = field.numpy()
+
+        snap.close()
+    
+    def print_metadata(self):
+        '''
+        Print info about the snapshot.
+        
+        '''
+        print('Snapshot info:\n')
+        print('File path:', self.file_path)
+        print()
+        print('Domain shape:', self.shape)
+        print('Domain size:', self.size)
+        print()
+        print('z =', self.z)

changed interp to fix jacobian error

lya_fields/eos.py
@@ -14,7 +14,7 @@ class EOS_at_z:
     
     def __init__(self, z, rhob_cgs_conversion):
         '''
-        Read in the redshift and perform interpolations.
+        Read in the redshift and interpolate the function n_HI.
         
         PARAMETERS
         ----------
@@ -25,14 +25,12 @@ class EOS_at_z:
         ------------------
         z: redshift
         n: the function n(log10(rho), log10(T)), interpolated with RectBivariateSpline
-        n_logr, n_logt: the partial derivatives of n w.r.t. log10(rho) and log10(T),
-        respectively; both interpolated with interp2d
         
         '''
 
         self.z = float(z)
         
-        ## interpolate the function n_HI and its two 1st-order partial derivatives
+        ## interpolate the function n_HI
         
         # set the ranges for interpolation
         length = 200 
@@ -49,16 +47,15 @@ class EOS_at_z:
             for j in range(length):
                 nhi_grid[i, j] = self.nyx_eos(rhob_cgs_range[i], temp_range[j])
                 
-        # interpolate n
         deg = 3 # degree of spline; default is 3
         self.n = interp.RectBivariateSpline(log10_rho_range, log10_t_range, nhi_grid, kx=deg, ky=deg)
 
         # interpolate the 1st-order partial derivatives: n_[log10(rho)] and n_[log10(T)]
-        n_logr_grid = self.n(log10_rho_range, log10_t_range, dx=1, dy=0)
-        self.n_logr = interp.interp2d(log10_rho_range, log10_t_range, n_logr_grid)
+#         n_logr_grid = self.n(log10_rho_range, log10_t_range, dx=1, dy=0)
+#         self.n_logr = interp.interp2d(log10_rho_range, log10_t_range, n_logr_grid)
 
-        n_logt_grid = self.n(log10_rho_range, log10_t_range, dx=0, dy=1)
-        self.n_logt = interp.interp2d(log10_rho_range, log10_t_range, n_logt_grid)
+#         n_logt_grid = self.n(log10_rho_range, log10_t_range, dx=0, dy=1)
+#         self.n_logt = interp.interp2d(log10_rho_range, log10_t_range, n_logt_grid)
         
     def nyx_eos(self, rhob, temp):
         '''
@@ -103,30 +100,6 @@ class EOS_at_z:
 
         return tf.reshape(t, shape)
 
-    
-    def compute_dn_dlogx(self, log10_rhob, log10_temp, x):
-        '''
-        Compute n_logx at specific coordinates for (log10_rhob, log10_temp), where x 
-        is either rhob or temp.
-        
-        PARAMETERS
-        ----------
-        log10(rhob), log10(temp): floats
-        x: a string, either 'rhob' or 'temp' 
-        
-        '''
-                
-        if x == 'rhob':
-            return interp.dfitpack.bispeu(self.n_logr.tck[0], self.n_logr.tck[1], self.n_logr.tck[2], \
-                                        self.n_logr.tck[3], self.n_logr.tck[4], \
-                                        log10_rhob, log10_temp)[0]
-        elif x == 'temp':
-            return interp.dfitpack.bispeu(self.n_logt.tck[0], self.n_logt.tck[1], self.n_logt.tck[2], \
-                                        self.n_logt.tck[3], self.n_logt.tck[4], \
-                                        log10_rhob, log10_temp)[0]
-        else:
-            raise NameError("The argument x must be either 'rhob' or 'temp'")
-    
     @tf.custom_gradient
     def compute_nhi(self, log10_rhob, log10_temp):
         '''
@@ -149,63 +122,17 @@ class EOS_at_z:
 
             '''
             
-            # test interp.dfitpack.bispeu
-            try:
-                fake_rho, fake_t = [[-20,-19]], [[[4,3]]]
-                
-                filler = interp.dfitpack.bispeu(self.n_logr.tck[0], self.n_logr.tck[1], self.n_logr.tck[2], \
-                                              self.n_logr.tck[3], self.n_logr.tck[4], \
-                                            self.flatten(fake_rho), self.flatten(fake_t))[0]
-                print('interp.dfitpack.bispeu works!')
-            except Exception as err:
-                print('Error: interp.dfitpack.bispeu doesn\'t work')
-                print(sys.exc_info()[0])
-                
-            # test compute_dn_dlogx
-            try:
-                fake_rho = tf.constant([-20, -19])
-                fake_t = tf.constant([3, 4])
-                
-                print('fake_rho:', fake_rho)
-                print('fake_rho type:', type(fake_rho))
-                print('fake_rho shape:', tf.shape(fake_rho))
-                
-                filler = self.compute_dn_dlogx(self.flatten(fake_rho), self.flatten(fake_t), 'rhob')
-                print('compute_dn_dlogx works on 1D tensors!')
-            except Exception as err:
-                print('Error: compute_dn_dlogx doesn\'t work on 1D tensors')
-                print(sys.exc_info()[0])
-        
-#             try:
-#                 fake_rho = tf.constant([[-20, -19], [-20, -19]])
-#                 fake_t = tf.constant([[3, 4], [3, 4]])
-#                 filler = self.compute_dn_dlogx(self.flatten(fake_rho), self.flatten(fake_t), 'rhob')
-#                 print('compute_dn_dlogx works on 2D tensors!')
-#             except Exception as err:
-#                 print('Error: compute_dn_dlogx doesn\'t work on 2D tensors')
-#                 print(sys.exc_info()[0])
-
             # compute the tensors containing the dn/dlogx values 
-#             dn_dlogr = interp.dfitpack.bispeu(self.n_logr.tck[0], self.n_logr.tck[1], self.n_logr.tck[2], \
-#                                               self.n_logr.tck[3], self.n_logr.tck[4], \
-#                                             self.flatten(log10_rhob), self.flatten(log10_temp))[0]
-#             dn_dlogr = self.unflatten(dn_dlogr, log10_rhob.shape)
-
-#             dn_dlogt = interp.dfitpack.bispeu(self.n_logt.tck[0], self.n_logt.tck[1], self.n_logt.tck[2], \
-#                                               self.n_logt.tck[3], self.n_logt.tck[4], \
-#                                             self.flatten(log10_rhob), self.flatten(log10_temp))[0]
-#             dn_dlogt = self.unflatten(dn_dlogt, log10_rhob.shape)
-
-            dn_dlogr = self.compute_dn_dlogx(self.flatten(log10_rhob), self.flatten(log10_temp), 'rhob')
-            dn_dlogt = self.compute_dn_dlogx(self.flatten(log10_rhob), self.flatten(log10_temp), 'temp')
-
+            dn_dlogr = self.n(np.array(log10_rhob).flatten(), np.array(log10_temp).flatten(), dx=1, dy=0, grid=False)
+            dn_dlogt = self.n(np.array(log10_rhob).flatten(), np.array(log10_temp).flatten(), dx=0, dy=1, grid=False)
+            
             dn_dlogr = self.unflatten(dn_dlogr, log10_rhob.shape)
-            dn_dlogt = self.unflatten(dn_dlogt, log10_rhob.shape)
+            dn_dlogt = self.unflatten(dn_dlogt, log10_temp.shape)
             
             # compute the tensors containing the dn/dx values 
             dn_drho = tf.divide(dn_dlogr, 10**log10_rhob) / np.log(10)
             dn_dt = tf.divide(dn_dlogt, 10**log10_temp) / np.log(10)
-        
+            
             return upstream * dn_drho, upstream * dn_dt
         
         nhi = self.n(log10_rhob, log10_temp, grid=False) # this is a 3D ndarray

lya_fields/lya_fields.py
@@ -7,7 +7,6 @@ Write the number density of neutral hydrogen (nHI), real- and redshift-space opt
 import h5py
 import numpy as np
 import scipy.interpolate as interp
-import sys
 import tensorflow as tf
 import time
 
@@ -18,10 +17,12 @@ import snapshot
 from spectrum import gmlt_spec_od_grid
 from constants import *
 
-# subsection shape
-shape = [1, 1024, 1024]
+save_grads = False
+
+## subsection shape
+shape = [1, 1, 1024]
     
-# define dataset paths
+## define dataset paths
 ds_path_rhob = "/native_fields/baryon_density"
 ds_path_temp = "/native_fields/temperature"
 ds_path_vx   = "/native_fields/velocity_x"
@@ -31,6 +32,19 @@ ds_path_vz   = "/native_fields/velocity_z"
 ds_path_nhi      = "/derived_fields/HI_number_density"
 ds_path_tau_real = "/derived_fields/tau_real"
 ds_path_tau_red  = "/derived_fields/tau_red"
+
+## define paths to store the gradients 
+
+# should have the same shape as the input fields
+path_dn_dr = "/n_gradients/dn_dr"
+path_dn_dt = "/n_gradients/dn_dt"
+
+# should have the shape [shape, shape],
+# e.g. [1, 1, 1024, 1, 1, 1024] for one skewer
+path_dtreal_dr = "/tau_jacobians/dtreal_drho"
+path_dtred_dr = "/tau_jacobians/dtred_drho"
+path_dtreal_dt = "/tau_jacobians/dtreal_dt"
+path_dtred_dt = "/tau_jacobians/dtred_dt"
 
 def write_field(field, dset_path, file_path):
     '''
@@ -93,6 +107,10 @@ def set_nhi(snap, rhob, temp):
     return grid.Grid(nhi_field, rhob.shape, rhob.size)
 
 def main():
+    
+    # track routine durations
+    times = open(t_path, 'w')
+    
     start1 = time.time() # track the total duration
     
     ### load in the density and temperature fields as grids ###
@@ -111,40 +129,55 @@ def main():
     t_path = 'times/times' + dims_str + '.txt'
     results_path = 'results/tf_fields' + dims_str + '.h5'
     
-    rhob = snap.read_subfield(ds_path_rhob, shape)
-    temp = snap.read_subfield(ds_path_temp, shape)
-    
-    # track routine durations
-    times = open(t_path, 'w')
-    
-    ### compute nhi grid ###
-    start = time.time()
-    nhi = set_nhi(snap, rhob, temp)
-    times.write('EOS duration = ' + str(time.time() - start) + '\n')
-    
-    write_field(nhi.field, 'nhi', results_path)
-    
-    ### calculate optical depth fields ###
-    # real-space tau
-    vpara = grid.Grid(tf.zeros(rhob.shape, dtype='float64'), rhob.shape, rhob.size)
-    
-    start = time.time()
-    tau_real = gmlt_spec_od_grid(snap.universe, snap.z, nhi.size,
-            nhi.field, temp.field, vpara.field, nhi.field.shape[2])
-    times.write('tau_real duration = ' + str(time.time() - start) + '\n')
-    
-    write_field(tau_real.field, 'tau_real', results_path)
+    with tf.GradientTape(persistent=True) as tape:
+        rhob = snap.read_subfield(ds_path_rhob, shape)
+        temp = snap.read_subfield(ds_path_temp, shape)
 
-    # redshift-space tau
-    #vpara = snap.read_field(ds_path_vz)
-    vpara = snap.read_subfield(ds_path_vz, shape)
-    
-    start = time.time()
-    tau_red = gmlt_spec_od_grid(snap.universe, snap.z, nhi.size,
-            nhi.field, temp.field, vpara.field, nhi.field.shape[2])
-    times.write('tau_red duration = ' + str(time.time() - start) + '\n')
+        ### compute nhi grid ###
+        start = time.time()
+        nhi = set_nhi(snap, rhob, temp)
+        times.write('EOS duration = ' + str(time.time() - start) + '\n')
 
-    write_field(tau_red.field, 'tau_red', results_path)
+        write_field(nhi.field, 'nhi', results_path)
+
+        ### calculate optical depth fields ###
+        # real-space tau
+        vpara = grid.Grid(tf.zeros(rhob.shape, dtype='float64'), rhob.shape, rhob.size)
+
+        start = time.time()
+        tau_real = gmlt_spec_od_grid(snap.universe, snap.z, nhi.size,
+                nhi.field, temp.field, vpara.field, nhi.field.shape[2])
+        times.write('tau_real duration = ' + str(time.time() - start) + '\n')
+
+        write_field(tau_real.field, 'tau_real', results_path)
+
+        # redshift-space tau
+        vpara = snap.read_subfield(ds_path_vz, shape)
+
+        start = time.time()
+        tau_red = gmlt_spec_od_grid(snap.universe, snap.z, nhi.size,
+                nhi.field, temp.field, vpara.field, nhi.field.shape[2])
+        times.write('tau_red duration = ' + str(time.time() - start) + '\n')
+
+        write_field(tau_red.field, 'tau_red', results_path)
+    
+    if save_grads:
+        dn_dr = tape.gradient(nhi.field, rhob.field)
+        dn_dt = tape.gradient(nhi.field, temp.field)
+        write_field(dn_dr, path_dn_dr, results_path)
+        write_field(dn_dt, path_dn_dt, results_path)
+        
+        dtreal_dr = tape.jacobian(tau_real.field, rhob.field)
+        dtred_dr = tape.jacobian(tau_red.field, rhob.field)
+        
+        # note: for one skewer, the below lines may throw an out-of-memory error
+        dtreal_dt = tape.jacobian(tau_real.field, temp.field)
+        dtred_dt = tape.jacobian(tau_red.field, temp.field)
+        
+        write_field(dtreal_dr, path_dtreal_dr, results_path)
+        write_field(dtred_dr, path_dtred_dr, results_path)
+        write_field(dtreal_dt, path_dtreal_dt, results_path)
+        write_field(dtred_dt, path_dtred_dt, results_path)
     
     times.write('Total duration = ' + str(time.time() - start1) + '\n')
     times.close()